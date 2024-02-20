604cf2b98b01f1079c404af687305d7af12dd044
Avraam Marimpis <makhsm@gmail.com>
2024-02-20 19:05:26 +0100

    Refactor tables and materialized views.

--- src/2_prepare_streaming_tables.sql
+++ src/2_prepare_streaming_tables.sql
@@ -1,58 +1,66 @@
 CREATE TABLE IF NOT EXISTS house.queue
 (
-    artist String,
-    song String,
-    duration String,
-    ts String,
-    sessionId String,
-    auth String,
-    level String,
+    artist        String,
+    song          String,
+    duration      String,
+    ts            String,
+    sessionId     String,
+    auth          String,
+    level         String,
     itemInSession String,
-    city String,
-    zip String,
-    state String,
-    userAgent String,
-    lon String,
-    lat String,
-    userId String,
-    lastName String,
-    firstName String,
-    gender String,
-    registration String
+    city          String,
+    zip           String,
+    state         String,
+    userAgent     String,
+    lon           String,
+    lat           String,
+    userId        String,
+    lastName      String,
+    firstName     String,
+    gender        String,
+    registration  String
 )
-ENGINE=Kafka()
-SETTINGS
-    kafka_broker_list = 'kafka:29092',
-    kafka_topic_list = 'listen_events',
-    kafka_group_name = 'click-group',
-    kafka_format = 'JSONEachRow';
-
-DROP TABLE house.events;
+    ENGINE = Kafka()
+        SETTINGS
+            kafka_broker_list = 'kafka:29092',
+            kafka_topic_list = 'listen_events',
+            kafka_group_name = 'click-group',
+            kafka_format = 'JSONEachRow';
 
 CREATE TABLE IF NOT EXISTS house.events
 (
-    artist String,
-    song String,
-    duration Float32,
-    ts UInt128,
-    sessionId UInt32,
-    auth String,
-    level String,
+    artist        String,
+    song          String,
+    duration      Float32,
+    ts            UInt128,
+    sessionId     UInt32,
+    auth          String,
+    level         String,
     itemInSession UInt32,
-    city String,
-    zip String,
-    state String,
-    userAgent String,
-    lon Float32,
-    lat Float32,
-    userId UInt32,
-    lastName String,
-    firstName String,
+    city          String,
+    zip           String,
+    state         String,
+    userAgent     String,
+    lon           Float32,
+    lat           Float32,
+    userId        UInt32,
+    lastName      String,
+    firstName     String,
     gender Enum8(''=0, 'M'=1, 'F'=2),
-    registration UInt128
+    registration  UInt128
 )
-ENGINE = ReplacingMergeTree()
-ORDER BY (ts)
-PARTITION BY (city, state, zip);
+    ENGINE = ReplacingMergeTree()
+        ORDER BY (ts)
+        PARTITION BY (city, state, zip);
+
+CREATE TABLE IF NOT EXISTS house.daily_artists_count
+(
+    date   Date,
+    artist String,
+    plays  UInt128
+)
+    ENGINE = SummingMergeTree()
+        ORDER BY date
+        PARTITION BY (date, artist);
 
 
--- src/3_prepare_mvs.sql
+++ src/3_prepare_mvs.sql
@@ -1,26 +1,35 @@
-DROP TABLE house.events_mv;
-
-CREATE MATERIALIZED VIEW house.events_mv TO house.events
+CREATE MATERIALIZED VIEW house.mv_events TO house.events
 AS
-    SELECT
-        artist,
-        song,
-        toFloat32(duration) AS duration,
-        toUInt128(ts) AS ts,
-        toUInt32(sessionId) AS sessionId,
-        auth,
-        level,
-        toUInt32(itemInSession) AS itemInSession,
-        city,
-        zip,
-        state,
-        userAgent,
-        toFloat32(lon) AS lon,
-        toFloat32(lat) AS lat,
-        toUInt32(userId) AS userId,
-        lastName,
-        firstName,
-        CAST(gender, 'Enum8(\'\'=0, \'M\'=1, \'F\'=2)') AS gender,
-        toUInt128(registration) AS registration
-    FROM house.queue
+SELECT artist,
+       song,
+       toFloat32(duration)                             AS duration,
+       toUInt128(ts)                                   AS ts,
+       toUInt32(sessionId)                             AS sessionId,
+       auth,
+       level,
+       toUInt32(itemInSession)                         AS itemInSession,
+       city,
+       zip,
+       state,
+       userAgent,
+       toFloat32(lon)                                  AS lon,
+       toFloat32(lat)                                  AS lat,
+       toUInt32(userId)                                AS userId,
+       lastName,
+       firstName,
+       CAST(gender, 'Enum8(\'\'=0, \'M\'=1, \'F\'=2)') AS gender,
+       toUInt128(registration)                         AS registration
+FROM house.queue;
+
+DROP TABLE house.mv_daily_artists_count;
+
+CREATE MATERIALIZED VIEW house.mv_daily_artists_count TO house.daily_artists_count
+AS
+SELECT
+--         formatDateTime(fromUnixTimestamp(ts), '%Y-%m-%d') AS date,
+       DISTINCT(artist) AS artist,
+       COUNT(*)                                        AS plays
+FROM house.mv_events
+GROUP BY artist
+ORDER BY plays DESC, artist ASC;
 
